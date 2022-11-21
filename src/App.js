b32c7cf27307228cfcf5e05b37ae990ae6703b09
gnahc2305 <achangmurillo@gmail.com>
2022-11-21 18:23:50 -0500

    highlight goals

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import { useState } from 'react';
-import './App.css';
-import Header from './components/Header';
-import Info from './components/Info';
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Info from "./components/Info";
 
 function App() {
   const [score, setScore] = useState(0);
+  const [c1, setC1] = useState("white");
+  const [c2, setC2] = useState("white");
+  const [c3, setC3] = useState("white");
 
-  function addScore() {
+  function addScore(e) {
     setScore(score + 1);
+
+    if (e.target.className === "dog") {
+      setC1("red");
+    } else if (e.target.className === "boots") {
+      setC2("red");
+    } else if (e.target.className === "cookie") {
+      setC3("red");
+    }
   }
 
   function winGame() {
     if (score === 3) {
-      console.log('You Win!');
+      alert("You Win!");
     }
   }
 
@@ -21,12 +32,11 @@ function App() {
   return (
     <div className="app">
       <Header score={score} />
-      <Info />
+      <Info color1={c1} color2={c2} color3={c3} />
 
-      <div className="dog"></div>
-      <div className="boots"></div>
-      <div className="cookie"></div>
-
+      <div className="dog" onClick={addScore}></div>
+      <div className="boots" onClick={addScore}></div>
+      <div className="cookie" onClick={addScore}></div>
     </div>
   );
 }
--- src/components/Info.js
+++ src/components/Info.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-function Info() {
+function Info({ color1, color2, color3}) {
   return (
     <div className='info'>
         <ol>
-            <li>A white dog (Dalmata) </li>
-            <li>Red boots</li>
-            <li>A cookie man</li>
+            <li style={{color: color1}}>A white dog (Dalmata) </li>
+            <li style={{color: color2}}>Red boots</li>
+            <li style={{color: color3}}>A cookie man</li>
         </ol>
     </div>
   )
