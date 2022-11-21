8ece7c481fdfc2d8ab9e39368726f807e2ea002d
gnahc2305 <achangmurillo@gmail.com>
2022-11-21 00:00:42 -0500

    create header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import { useState } from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
+  const [score, setScore] = useState(0);
+
+  function addScore() {
+    setScore(score + 1);
+  }
+
+  function winGame() {
+    if (score === 3) {
+      console.log('You Win!');
+    }
+  }
+
+  winGame();
+
   return (
     <div className="app">
-      <div className="person1">
-        
+      <Header />
+      <div className="person1" onClick={addScore}>
+
       </div>
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='header'>
+        <h1>Photo Tagging App</h1>
+        <h2>Score: 0</h2>
+    </div>
+  )
+}
+
+export default Header
